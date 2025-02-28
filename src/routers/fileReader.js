7828a61dd5b663953eee1e78e42463f235662bb4
Kingterron <gianlucafiore27@gmail.com>
2025-02-28 09:36:37 +0100

    Router per la lettura dei file

--- src/routers/fileReader.js
+++ src/routers/fileReader.js
@@ -5,7 +5,10 @@ const router = new express.Router();
 
 const filesPath = path.join(__dirname, '../JsonFiles');
 
-router.get('/readFiles', (req,res, next) => {
+
+let html = null
+
+router.get('/readFiles', (req,res) => {
     fs.readdir(filesPath, function(err,files) {
         if (err) { 
             return res.status(404).send({error: "No file found"}); 
@@ -14,5 +17,31 @@ router.get('/readFiles', (req,res, next) => {
     })
 })
 
+router.post('/createFromFile', async (req,res) => {
+    //console.log(req)
+    let perc = (path.join(filesPath, req.body.chooseFile));
+    
+    fs.readFile(perc , (err,data) => {
+        /* try {
+            formData = JSON.parse(data)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({error: 'Internal server error'})
+        }  */
+
+            formData = JSON.parse(data)
+            const data = Object.assign({}, {
+                fields: [],
+                title: "Default title"
+            }, formData)
+            if (!(formData && formData.fields && formData.title)) {
+                return res.status(400).json({error: 'Json format is not valid'})
+            }
+
+            res.render('index', {title: formData.title, fields: formData.fields}, (error,form) => {
+                res.send(form)
+            })
+    })   
+})
 
 module.exports = router
